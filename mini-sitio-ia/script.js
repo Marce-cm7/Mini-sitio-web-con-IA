16a4f31d47f03b18968511d30a5a27ebefbfdca8
Marcela Collado <marcela.colladom@gmail.com>
2026-08-23 12:53:25 -0600

    Se añadío variables y contaenación de JS

--- mini-sitio-ia/script.js
+++ mini-sitio-ia/script.js
@@ -1,17 +1,34 @@
 
+//Guarda el nombre del primer parque
 let parque1 = "DisneyLand Park";
+
+//Guarda el nombre del segundo parque
 let parque2 = "Disney California Adventure Park";
 
+// Guarda la cantidad total de parques que hay en Anaheim.
 let cantidadDeParques = 2;
+
+//Guarda la cantidad total de atracciones que recomiendo en mi sitio web.
 let numeroTotalDeAtracciones = 6;
 
 // ¿Es verdad que ambos parques se enuentran uno enfrente del otro?
 let parquesEnfrenteDelOtro = true;
 
+
+// Muestra en la consola el nombre del primer parque.
 console.log(parque1);
+
+// Muestra en la consola el nombre del segundo parque.
 console.log(parque2);
 
+//Muestra en la consola la cantidad de parques.
 console.log(cantidadDeParques);
+
+//Muestra en la consola el número total de atracciones recomendadas.
 console.log(numeroTotalDeAtracciones);
 
+//Muestra en la consola si los parques se encuentran uno enfrente del otro.
 console.log(parquesEnfrenteDelOtro)
+
+//Concatenación usnado algunos 
+console.log( "Hola! En este sitio web, que hice con amor, te recomiendo " + numeroTotalDeAtracciones + " atracciones " + "entre los " + cantidadDeParques + " parques de Disney que hay en Anaheim: " + parque1 + " y " + parque2 + " ¡Espero que te sirvan en un futuro!");
