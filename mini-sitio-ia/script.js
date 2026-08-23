ddde0961c40e470786ddffe43c78cbb840d61842
Marcela Collado <marcela.colladom@gmail.com>
2026-08-23 12:32:26 -0600

    Se estilizó el navbar y footer, entre otras cosas

---
+++ mini-sitio-ia/script.js
@@ -0,0 +1,17 @@
+
+let parque1 = "DisneyLand Park";
+let parque2 = "Disney California Adventure Park";
+
+let cantidadDeParques = 2;
+let numeroTotalDeAtracciones = 6;
+
+// ¿Es verdad que ambos parques se enuentran uno enfrente del otro?
+let parquesEnfrenteDelOtro = true;
+
+console.log(parque1);
+console.log(parque2);
+
+console.log(cantidadDeParques);
+console.log(numeroTotalDeAtracciones);
+
+console.log(parquesEnfrenteDelOtro)
